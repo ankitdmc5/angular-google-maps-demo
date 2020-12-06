b04378ab3bd5a0638cdc20f5e76e85666ab11050
ankitdmc5 <ankitdmc5@gmail.com>
2020-12-06 13:56:41 +0530

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,61 +1,71 @@
-import { Component } from '@angular/core';
-import { MouseEvent } from '@agm/core';
+import { Component } from "@angular/core";
+import { MouseEvent } from "@agm/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent {
   // google maps zoom level
-  zoom: number = 8;
-  
-  // initial center position for the map
-  lat: number = 51.673858;
-  lng: number = 7.815982;
+  zoom: number = 12;
 
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
+  // initial center position for the map
+  lat: number = 18.57509;
+  lng: number = 73.741568;
+  selectedMarker: marker;
+  infoWindowOpened = null;
+  previous_info_window = null;
+
+  clickedMarker(label: string, index: number, infoWindow) {
+    this.selectedMarker = this.markers[index];
+    if (this.previous_info_window == null)
+      this.previous_info_window = infoWindow;
+    else {
+      this.infoWindowOpened = infoWindow;
+      this.previous_info_window.close();
+    }
+    this.previous_info_window = infoWindow;
+    console.log(`clicked the marker: ${label || index}`);
   }
-  
-  mapClicked($event: MouseEvent) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
-    });
-  }
-  
+
   markerDragEnd(m: marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
+    console.log("dragEnd", m, $event);
   }
-  
+
+close_window(){
+if (this.previous_info_window != null ) {
+  this.previous_info_window.close()
+  }    
+}
+
+// will fetch these from backend calls
   markers: marker[] = [
-	  {
-		  lat: 51.673858,
-		  lng: 7.815982,
-		  label: 'A',
-		  draggable: true
-	  },
-	  {
-		  lat: 51.373858,
-		  lng: 7.215982,
-		  label: 'B',
-		  draggable: false
-	  },
-	  {
-		  lat: 51.723858,
-		  lng: 7.895982,
-		  label: 'C',
-		  draggable: true
-	  }
-  ]
+    {
+      lat: 18.575097701618027,
+      lng: 73.74156811898027,
+      label: "A",
+      draggable: false,
+      popupText: "kit ka ghar in danger red",
+      severity: 'danger',
+    },
+    {
+      lat: 18.592376572786087,
+      lng: 73.70653930689984,
+      label: "B",
+      draggable: false,
+      popupText: "shruti ka ghar in fine blue",
+      severity: 'fine',
+    }
+  ];
 }
 
 // just an interface for type safety.
 interface marker {
-	lat: number;
-	lng: number;
-	label?: string;
-	draggable: boolean;
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+  popupText: string;
+  severity?: string;
 }
